d2301183726db3e352b0485f8d486097934bbd3a
Bruno Goncalves <bugotech@gmail.com>
2018-07-24 12:03:46 -0300

    inicio do Aws SQS

--- src/QueueServiceProviders.php
+++ src/QueueServiceProviders.php
@@ -1,5 +1,7 @@
 <?php namespace Nano7\Queue;
 
+use Aws\Sqs\SqsClient;
+use Nano7\Queue\Queues\SqsQueue;
 use Nano7\Foundation\Support\ServiceProvider;
 
 class QueueServiceProviders extends ServiceProvider
@@ -9,6 +11,44 @@ class QueueServiceProviders extends ServiceProvider
      */
     public function register()
     {
-        //...
+        $this->registerManager();
+    }
+
+    /**
+     * Register manager.
+     */
+    protected function registerManager()
+    {
+        $this->app->singleton('queue', function () {
+            $manager = new QueueManager($this->app, $this->app['config']->get('queue', []));
+
+            // Driver Aws SQS
+            $this->registerAwsSqs($manager);
+
+            return $manager;
+        });
+    }
+
+    /**
+     * Register driver AWS SQS.
+     *
+     * @param QueueManager $manager
+     */
+    protected function registerAwsSqs(QueueManager $manager)
+    {
+        $manager->extend('sqs', function($app, $config) {
+
+            $config = array_merge([
+                'version' => 'latest',
+                'http' => [
+                    'timeout' => 60,
+                    'connect_timeout' => 60,
+                ],
+            ], $config);
+
+            $client = new SqsClient($config);
+
+            return new SqsQueue($app, $client, $config['queue'], $config['prefix']);
+        });
     }
 }
